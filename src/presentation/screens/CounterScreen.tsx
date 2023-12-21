40d333a73a512e698ada33f0bfbc8d214f74d0de
ccastillob <ccastillo993a@gmail.com>
2023-12-21 00:28:20 -0500

    feat: create counter component

---
+++ src/presentation/screens/CounterScreen.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { PrimaryButton } from '../components';
+import { Button } from 'react-native-paper';
+
+export const CounterScreen = () => {
+
+  const [count, setCount] = useState(10);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{count}</Text>
+
+      {/* <PrimaryButton
+        label="Increment"
+        onLongPress={() => setCount(0)}
+        onPress={() => setCount(count + 1)}
+      /> */}
+
+      <Button
+        onLongPress={() => setCount(0)}
+        onPress={() => setCount(count + 1)}
+        mode="contained"
+      >
+        Incrementar
+      </Button>
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 80,
+    color: 'black',
+    fontWeight: '300',
+  },
+});
+
+
