06c28a298452b99f8da4f3b9c26d500be2a68f07
ccastillob <ccastillo993a@gmail.com>
2023-12-21 00:25:32 -0500

    feat: add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'react/self-closing-comp': 'off',
+    'react-native/no-inline-styles': 0,
+    '@typescript-eslint/no-unused-vars': 'off',
+    'prettier/prettier': 0,
+  },
 };
