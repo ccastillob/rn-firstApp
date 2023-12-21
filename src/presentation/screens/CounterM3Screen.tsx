16bca3ae06db1d5ed12a480c474aac0790608fd7
ccastillob <ccastillo993a@gmail.com>
2023-12-21 00:41:51 -0500

    feat: implement global icons

--- src/presentation/screens/CounterM3Screen.tsx
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import { Text, View } from 'react-native';
 import { globalStyles } from '../theme/global.styles';
 
-import Icon from 'react-native-vector-icons/Ionicons';
-
 import { FAB } from 'react-native-paper';
 
 export const CounterM3Screen = () => {
@@ -14,14 +12,15 @@ export const CounterM3Screen = () => {
     <View style={globalStyles.centerContainer}>
       <Text style={globalStyles.title}>{count}</Text>
 
-      <Icon name="accessibility-outline" size={25} />
+      {/* <Icon name="accessibility-outline" size={25} /> */}
 
 
       <FAB
-        label="+1"
+        // label="+1"
         style={globalStyles.fab}
         onPress={() => setCount(count + 1)}
         onLongPress={() => setCount(0)}
+        icon="add-outline"
       />
     </View>
   );
