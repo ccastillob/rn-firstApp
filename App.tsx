48676d76aa60757efcfd488f79bbd6b307fee628
ccastillob <ccastillo993a@gmail.com>
2023-12-22 01:20:50 -0500

    feat: learn flex box

--- App.tsx
+++ App.tsx
@@ -8,6 +8,8 @@ import IonIcon from 'react-native-vector-icons/Ionicons';
 import { BoxObjectModelScreen } from './src/presentation/screens/BoxObjectModelScreen';
 import { DimensionScreen } from './src/presentation/screens/DimensionScreen';
 import { PositionScreen } from './src/presentation/screens/PositionScreen';
+import { FlexScreen } from './src/presentation/screens/FlexScreen';
+import { FlexDirectionScreen } from './src/presentation/screens/FlexDirectionScreen';
 
 export const App = () => {
   return (
@@ -22,7 +24,9 @@ export const App = () => {
         {/* <CounterM3Screen /> */}
         {/* <BoxObjectModelScreen /> */}
         {/* <DimensionScreen /> */}
-        <PositionScreen />
+        {/* <PositionScreen /> */}
+        {/* <FlexScreen /> */}
+        <FlexDirectionScreen />
       </SafeAreaView>
     </PaperProvider>
   );
